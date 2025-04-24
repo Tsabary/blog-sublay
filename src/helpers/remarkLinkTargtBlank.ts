12d3e1fd98efb8b2cd15705b7299160b5d987e36
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-24 10:47:37 +0300

    push

--- src/helpers/remarkLinkTargtBlank.ts
+++ src/helpers/remarkLinkTargtBlank.ts
@@ -4,8 +4,8 @@ import { Plugin } from "unified";
 const remarkLinkTargetBlank: Plugin = () => {
   return (tree) => {
     visit(tree, "link", (node: any) => {
-      if (!node.data) node.data = {};
-      if (!node.data.hProperties) node.data.hProperties = {};
+      node.data ||= {};
+      node.data.hProperties ||= {};
       node.data.hProperties.target = "_blank";
       node.data.hProperties.rel = "noopener noreferrer";
     });
