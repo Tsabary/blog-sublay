a67915facb68819d46ad01446b53a8ae0a38661d
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-27 08:52:30 +0300

    push

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -8,8 +8,10 @@ function ArticlesPage() {
   return (
     <Layout>
       <FeedProvider resourceId="blog" limit={9} sortBy="new">
-        <div className="pt-24 px-8">
-          <NavigateHomeButton />
+        <div className="pt-24 flex flex-col items-center px-8">
+          <div className="w-full max-w-6xl mx-auto">
+            <NavigateHomeButton />
+          </div>
         </div>
         <ArticlesGrid />
         {/* <Subscribe /> */}
