7884631a2bec34b619289b53ff35167052ddb8de
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-27 13:49:20 +0200

    update auth

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -139,6 +139,25 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, []);
 
+    const handleGoogleLogin = async () => {
+    try {
+      window.location.href =
+        process.env.NEXT_PUBLIC_SERVER_URL + "/internal/clients-auth/google";
+    } catch (err: unknown) {
+      handleError(err, "Failed to initiate Google login: ");
+    }
+  };
+
+  const handleGithubLogin = async () => {
+    try {
+      window.location.href =
+        process.env.NEXT_PUBLIC_SERVER_URL + "/internal/clients-auth/github";
+    } catch (err: unknown) {
+      handleError(err, "Failed to initiate GitHub login: ");
+    }
+  };
+
+  
   const deleteAccount = async () => {};
 
   async function updateUserProfile(): Promise<void> {}
@@ -169,6 +188,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         updateUserProfile,
         getNewAccessToken,
         accessToken,
+        handleGoogleLogin,
+        handleGithubLogin,
       }}
     >
       {!loadingInitial && children}
